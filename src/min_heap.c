ed8eba291b93669b41fc8b38400c5915cdff486d
Jerry Ryle <jerry@ryle.io>
2022-08-12 14:18:17 -0700

    Completed MinHeap implementation.

--- src/min_heap.c
+++ src/min_heap.c
@@ -2,13 +2,24 @@
 #include <assert.h>
 #include <stdlib.h>
 
+// Given an element index 'i', calculate the index of the parent element.
+#define PARENT_ELEMENT(i)   (((i)-1) / 2)
+
+// Given an element index 'i', calculate the index of the left child element.
+#define LEFT_CHILD_ELEMENT(i)     ((2*(i)) + 1)
+
+// Given an element index 'i', calculate the index of the right child element.
+#define RIGHT_CHILD_ELEMENT(i)    ((2*(i)) + 2)
+
 struct min_heap {
-    char *data;
+    struct min_heap_element *data;
     size_t element_count;
     size_t element_capacity;
-    size_t element_size;
     int (*compare)(const void *, const void *);
 };
+
+static void heapify(struct min_heap *heap);
+static void swap_elements(struct min_heap *heap, size_t first_element, size_t second_element);
 
 struct min_heap *min_heap_new(void *data, size_t data_size)
 {
@@ -22,7 +33,7 @@ struct min_heap *min_heap_new(void *data, size_t data_size)
     if (!heap) {
         return NULL;
     }
-    heap->data = (char *)data;
+    heap->data = (struct min_heap_element *)data;
     heap->element_count = 0;
     heap->element_capacity = data_size/sizeof(struct min_heap_element);
 
@@ -44,10 +55,111 @@ size_t min_heap_count(struct min_heap const *heap)
 bool min_heap_add(struct min_heap *heap, uint32_t key, FILE *value)
 {
     assert(heap);
+    if (heap->element_count >= heap->element_capacity) {
+        return false;
+    }
+
+    // Get the index for a new element at the end of the array
+    size_t current_element = heap->element_count;
+    // Increase the element count to account for the new element
+    heap->element_count++;
+
+    // Put the new element data at the end of the array.
+    heap->data[current_element].key = key;
+    heap->data[current_element].value = value;
+
+    // As long as the current element is smaller than its parent
+    // Bubble it upwards until we hit the top of the tree
+    while ( (current_element != 0) &&
+            (heap->data[current_element].key < heap->data[PARENT_ELEMENT(current_element)].key) ) {
+        // If the current element is smaller than its parent, swap it and keep moving upwards.
+        size_t parent_element = PARENT_ELEMENT(current_element);
+        swap_elements(heap, current_element, parent_element);
+        current_element = parent_element;
+    }
+    return true;
+}
+
+bool min_heap_pop(struct min_heap *heap, uint32_t *key, FILE **value)
+{
+    assert(heap);
+    assert(key);
+    assert(value);
+    if (heap->element_count > 0) {
+        // Retrieve element's value
+        *key = heap->data[0].key;
+        *value = heap->data[0].value;
+
+        // If there's more than one element, re-heapify. Otherwise, just decrement the count and return.
+        if (heap->element_count > 1) {
+            // Move the last element to the top
+            heap->data[0] = heap->data[heap->element_count-1];
+            // re-heapify
+            heapify(heap);
+        }
+        heap->element_count--;
+        return true;
+    }
     return false;
+}
+
+void min_heap_clear(struct min_heap *heap)
+{
+    // Set the number of elements to zero. This effectively clears the heap.
+    heap->element_count = 0;
 }
 
 void min_heap_delete(struct min_heap *heap)
 {
     free(heap);
+}
+
+static void heapify(struct min_heap *heap)
+{
+    // If there's zero elements or only one element in the heap, there's nothing to do.
+    if (heap->element_count <= 1) {
+        return;
+    }
+
+    // Start with the top of the heap
+    size_t current_element = 0;
+    // Setting this to the last element just ensures that we enter the loop below.
+    size_t min_element = heap->element_count - 1;
+
+    // Keep moving downwards until the current element is the minimum element.
+    while (current_element != min_element) {
+        // Begin by assuming the current is the min element.
+        min_element = current_element;
+
+        // Calculate the array indices of the left and right elements
+        size_t const left_element = LEFT_CHILD_ELEMENT(current_element);
+        size_t const right_element = RIGHT_CHILD_ELEMENT(current_element);
+
+        // If there's a left child element, and if it's smaller than the current element, capture the left child element
+        // as the minimum.
+        if ((left_element < heap->element_count) && (heap->data[left_element].key < heap->data[current_element].key)) {
+            min_element = left_element;
+        }
+        // If there's a right child element, and if it's smaller than both the current element and the left child
+        // element, capture the right child element as the minimum.
+        if ((right_element < heap->element_count) && (heap->data[right_element].key < heap->data[min_element].key)) {
+            min_element = right_element;
+        }
+
+        // If the current element is not the smallest element, swap it with the smallest and proceed downwards.
+        if (min_element != current_element) {
+            swap_elements(heap, min_element, current_element);
+
+            // The min element has been swapped with the current element. Update current_element such that we perform
+            // the next iteration starting at the position of the old min_element.
+            current_element = min_element;
+        }
+    }
+}
+
+static void swap_elements(struct min_heap *heap, size_t first_element, size_t second_element)
+{
+    struct min_heap_element const temp_element = heap->data[first_element];
+    heap->data[first_element] = heap->data[second_element];
+    heap->data[second_element] = temp_element;
 }
--- tests/min_heap_test.cpp
+++ tests/min_heap_test.cpp
@@ -38,3 +38,160 @@ TEST_F(MinHeapTest, CannotCreateHeapWithInsufficientDataSize) {
     struct min_heap *heap = min_heap_new(buffer.data(), buffer.size());
     EXPECT_TRUE(heap == nullptr);
 }
+
+TEST_F(MinHeapTest, CannotAddMoreElementsThanHeapCapacity) {
+    size_t capacity = min_heap_capacity(heap);
+    for (size_t i = 0; i < capacity; i++) {
+        EXPECT_TRUE(min_heap_add(heap, (uint32_t)i, nullptr));
+    }
+    EXPECT_FALSE(min_heap_add(heap, (uint32_t)capacity, nullptr));
+}
+
+TEST_F(MinHeapTest, CannotPopFromEmptyHeap) {
+    uint32_t key = 0;
+    FILE *value = nullptr;
+    EXPECT_FALSE(min_heap_pop(heap, &key, &value));
+}
+
+TEST_F(MinHeapTest, CanPopAddedElement) {
+    uint32_t key = 0;
+    FILE *value = nullptr;
+    EXPECT_TRUE(min_heap_add(heap, 42, (FILE *)0x12345678));
+    EXPECT_TRUE(min_heap_pop(heap, &key, &value));
+    EXPECT_EQ(key, 42);
+    EXPECT_EQ(value, (FILE *)0x12345678);
+}
+
+TEST_F(MinHeapTest, CannotPopMoreElementsThanAdded) {
+    uint32_t key = 0;
+    FILE *value = nullptr;
+    EXPECT_TRUE(min_heap_add(heap, 42, (FILE *)0x12345678));
+    EXPECT_TRUE(min_heap_pop(heap, &key, &value));
+    EXPECT_EQ(key, 42);
+    EXPECT_EQ(value, (FILE *)0x12345678);
+    EXPECT_FALSE(min_heap_pop(heap, &key, &value));
+}
+
+TEST_F(MinHeapTest, SmallestElementInsertedLastMovesToTopOfHeap) {
+    uint32_t key = 0;
+    FILE *value = nullptr;
+
+    // The heap has 0 elements
+    EXPECT_EQ(min_heap_count(heap), 0);
+
+    // 42 is added to the end of the heap and it becomes the top of heap since it's the only element.
+    EXPECT_TRUE(min_heap_add(heap, 42, (FILE *)0x00000001));
+    // 0 is added to the end of the heap and it becomes the top of heap since it's smaller than 42.
+    EXPECT_TRUE(min_heap_add(heap, 0, (FILE *)0x00000002));
+
+    // The heap now has 2 elements
+    EXPECT_EQ(min_heap_count(heap), 2);
+
+    // 0 is popped from the heap and 42 becomes the new top of heap.
+    EXPECT_TRUE(min_heap_pop(heap, &key, &value));
+    EXPECT_EQ(key, 0);
+    EXPECT_EQ(value, (FILE *)0x00000002);
+
+    // 42 is popped from the heap and the heap is now empty
+    EXPECT_TRUE(min_heap_pop(heap, &key, &value));
+    EXPECT_EQ(key, 42);
+    EXPECT_EQ(value, (FILE *)0x00000001);
+
+    // The heap is now empty
+    EXPECT_EQ(min_heap_count(heap), 0);
+}
+
+TEST_F(MinHeapTest, HeapIsMaintainedAsElementsAreAddedAndRemoved) {
+    uint32_t key = 0;
+    FILE *value = nullptr;
+
+    // The heap has 0 elements
+    EXPECT_EQ(min_heap_count(heap), 0);
+
+    // 43 is added to the heap and it becomes the top of heap since it's the only element.
+    /* Heap:
+             43
+    */
+    EXPECT_TRUE(min_heap_add(heap, 43, (FILE *)0x00000001));
+    // 42 is added to the end of the heap and, because it is smaller than the 43 at the top, it is swapped with 43.
+    /* Heap:
+             43               42
+            /        ->      /
+          42               43
+    */
+    EXPECT_TRUE(min_heap_add(heap, 42, (FILE *)0x00000002));
+    // 41 is added to the end of the heap and, because it is smaller than the 42 above it, it is swapped with 42.
+    /* Heap:
+             42             41
+            /  \     ->    /  \
+          43    41       43    42
+    */
+    EXPECT_TRUE(min_heap_add(heap, 41, (FILE *)0x00000003));
+    // 0 is added to the end of the heap and, it moves upwards to become the top of the heap
+    /* Heap:
+             42             41             0
+            /  \     ->    /  \     ->    /  \
+          43    41        0   42        41    42
+         /              /              /
+        0             43             43
+    */
+    EXPECT_TRUE(min_heap_add(heap, 0, (FILE *)0x00000004));
+
+    // The heap now has 4 elements
+    EXPECT_EQ(min_heap_count(heap), 4);
+
+    // 0 is popped and 43 is temporarily moves to the top and then pushed downwards.
+    /* Heap:
+             0              43             41
+            /  \     ->    /  \     ->    /  \
+          41    42       41    42       43    42
+         /
+        43
+    */
+    EXPECT_TRUE(min_heap_pop(heap, &key, &value));
+    EXPECT_EQ(key, 0);
+    EXPECT_EQ(value, (FILE *)0x00000004);
+
+    // 41 is popped and 42 moves to the top and stays there.
+    /* Heap:
+             41             42
+            /  \     ->    /
+          43    42       43
+    */
+    EXPECT_TRUE(min_heap_pop(heap, &key, &value));
+    EXPECT_EQ(key, 41);
+    EXPECT_EQ(value, (FILE *)0x00000003);
+
+    // 42 is popped from the heap and 43 moves to the top.
+    /* Heap:
+             42             43
+            /        ->
+          43
+    */
+    EXPECT_TRUE(min_heap_pop(heap, &key, &value));
+    EXPECT_EQ(key, 42);
+    EXPECT_EQ(value, (FILE *)0x00000002);
+
+    // 43 is popped from the heap and the heap is now empty
+    EXPECT_TRUE(min_heap_pop(heap, &key, &value));
+    EXPECT_EQ(key, 43);
+    EXPECT_EQ(value, (FILE *)0x00000001);
+
+    // The heap is now empty
+    EXPECT_EQ(min_heap_count(heap), 0);
+}
+
+TEST_F(MinHeapTest, CanClearHeap) {
+    size_t capacity = min_heap_capacity(heap);
+    for (size_t i = 0; i < capacity; i++) {
+        EXPECT_TRUE(min_heap_add(heap, (uint32_t)i, nullptr));
+    }
+    // Heap is full. Cannot add another element.
+    EXPECT_FALSE(min_heap_add(heap, (uint32_t)capacity, nullptr));
+
+    // Clear heap
+    min_heap_clear(heap);
+
+    // Can now add another element.
+    EXPECT_TRUE(min_heap_add(heap, (uint32_t)capacity, nullptr));
+}
